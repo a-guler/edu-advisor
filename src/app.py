805240fbdc79110151936c51331732c39037c52c
Çağatay Yusuf Ergüneş <69120641+kato386@users.noreply.github.com>
2024-02-25 15:40:34 +0300

    loyola quiz sitesinden soruları ve her sorunun weightlerine göre rastgele data  üreten kod yazıldı, bu datadan interest sorularını ilgilendiren model oluşturuldu ve bütün soruların cevaplarına göre tahmin yapan /score endpointi hazırlandı

---
+++ src/app.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, jsonify
+from modeling.outcomes_quiz import outcome_model as om
+from modeling.outcomes_quiz import prettify_output as p
+
+import pandas as pd
+import numpy as np
+import json
+import sys
+import pickle as cPickle
+import requests
+import final_questions as fq
+app = Flask(__name__)
+
+
+def load_model(filename):
+    
+    with open(filename,"rb") as f:
+        model = cPickle.load(f)
+    return model
+
+
+def make_field_dicts(classes, probas):
+    
+    field_dict = {}
+    for label, proba in zip(classes, probas.reshape(-1,)):
+        field_dict[label] = proba
+    return field_dict
+
+
+def parse_interest_ans(answer_list):
+    
+    for i, ans in enumerate(answer_list):
+        answer_list[i] = int(ans)
+    model_format_array = np.array(answer_list).reshape(1, -1)
+    return model_format_array
+
+
+def calculate_scores(answer_list):
+    
+    for i, ans in enumerate(answer_list):
+        answer_list[i] = float(ans)
+    np_arr = np.array(answer_list)
+    return np.sum(np_arr)
+
+
+@app.route('/score', methods=['POST'])
+def score():
+    quiz_answers = request.json
+    int_answers, risk_ans, inc_ans = (quiz_answers[0],
+                                      quiz_answers[1],
+                                      quiz_answers[2])
+    answer_array = parse_interest_ans(int_answers)
+    risk_score = calculate_scores(risk_ans)
+    inc_score = calculate_scores(inc_ans)
+    classes= [
+    "Creative Arts",
+    "Math, Sciences, and Engineering",
+    "Social Sciences",
+    "Public Service, Law, and Policy",
+    "Business and Communication"
+    ]
+    probas = model.predict_proba(answer_array)
+    print(probas)
+    field_dict = make_field_dicts(classes, probas)
+    final_df = om.calculate_final_prob(job_df,
+                                       risk_score,
+                                       inc_score,
+                                       field_dict
+                                       ).sort_values(by='final_probs',
+                                                     ascending=False)
+    pretty_columns = p.prettified_columns
+    final_columns = p.final_columns
+    pretty_df = p.prettify_final_output(final_df,
+                                        pretty_columns,
+                                        final_columns,
+                                        20)
+    print(pretty_df)
+    object_list=[]
+    for index,row in pretty_df.iterrows():
+        obj=p.row_to_object(row)
+        object_list.append(obj)
+    
+    return jsonify({'data': object_list})
+
+model = load_model('modeling/interest_quiz/model4.pkl')
+job_df = pd.read_csv('../data/abt_ver1.csv')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
